14d5948ea22a59edd5bb4980edf6322defb8f037
Sathishkumar AJ <ajsatix@gmail.com>
2021-10-29 00:58:18 +0530

    Create before_install.sh

---
+++ scripts/before_install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -e
+export PORT=3000
+export DB = mydb
+export DBUSER = postgres     
+export DBPASS = postgres
+export DBHOST = terraform-20211028174440270500000001.cvveddzalzq0.us-east-1.rds.amazonaws.com
+export DBPORT = 5432
+export PORT = 3000 
